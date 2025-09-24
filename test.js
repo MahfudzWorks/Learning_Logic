27f1f75bfa8d5232874750955eb7c6dc57ca0863
mahfudzalfa <mahfudzworksss@gmail.com>
2025-09-24 19:55:55 +0700

    commit soal 1 & 2

--- test.js
+++ test.js
@@ -1,42 +1,42 @@
 // Soal 1
 
-// const readline = require("readline");
+const readline = require("readline");
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// let keluarga = {
-//   kakak: "polindrom",
-//   adek: "polindrom",
-//   orangtua: "nonpolindrom",
-// };
+let keluarga = {
+  kakak: "polindrom",
+  adek: "polindrom",
+  orangtua: "nonpolindrom",
+};
 
-// rl.question("masukkan nama anggota keluarga (kakak/adek/orangtua):", (cari) => {
-//   if (keluarga[cari]) {
-//     console.log(cari + " = " + keluarga[cari]);
-//   } else {
-//     console.log("anggota keluarga tidak ditemukan");
-//   }
+rl.question("masukkan nama anggota keluarga (kakak/adek/orangtua):", (cari) => {
+  if (keluarga[cari]) {
+    console.log(cari + " = " + keluarga[cari]);
+  } else {
+    console.log("anggota keluarga tidak ditemukan");
+  }
 
-//   rl.close();
-// });
+  rl.close();
+});
 
 // Soal 2
 
-// let n = 5;
+let n = 5;
 
-// for (let i = 1; i <= n; i++) {
-//   console.log("*".repeat(i));
-// }
+for (let i = 1; i <= n; i++) {
+  console.log("*".repeat(i));
+}
 
-// for (let i = 1; i <= n; i++) {
-//   console.log(" ".repeat(n - i) + "*".repeat(i));
-// }
+for (let i = 1; i <= n; i++) {
+  console.log(" ".repeat(n - i) + "*".repeat(i));
+}
 
-// for (let i = 1; i <= n; i++) {
-//   console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
-// }
+for (let i = 1; i <= n; i++) {
+  console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+}
 
 // Soal 3
